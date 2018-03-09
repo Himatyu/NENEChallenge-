250546cea91a766eb8815bce04b8a4ab7e918159
暇厨 <fortis1019@yahoo.co.jp>
2018-03-10 01:13:34 +0900

    add mininum gamapad

--- NeneLabyrinth/Main.cpp
+++ NeneLabyrinth/Main.cpp
@@ -1,8 +1,11 @@
 #include <windows.h>
 #include "Core/Window.h"
 #include"Rendering\Graphics.h"
+
 using namespace NeneLabyrinth;
 
+#include<Xinput.h>
+#pragma comment (lib, "xinput.lib")
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 {
@@ -17,14 +20,33 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 	Rendering::Graphics graphics(window, 640, 480);
 	graphics.Initialize();
 
+	XINPUT_STATE state;
+	XINPUT_VIBRATION vib{ 10000 ,10000 };
+
 	while (!window.IsReceiveQuitMessage())
 	{
+
+		XInputGetState(0, &state);
+
+		if (state.Gamepad.wButtons & XINPUT_GAMEPAD_A)
+		{
+			XInputSetState(0, &vib);
+			XInputEnable(true);
+		}
+
+		if (state.Gamepad.wButtons & XINPUT_GAMEPAD_B)
+		{
+			break;
+		}
+
 		//DO Update
 		graphics.ClearBackBuffer();
 
 		//DO Rendring
 		graphics.Presetnt();
 	}
+
+	XInputEnable(false);
 
 	return 0;
 }
